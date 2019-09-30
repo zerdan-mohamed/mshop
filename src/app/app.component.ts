1ec59deab2e5b97852fad51e9b6e780172378e10
zerdan <zerdan.mohamed@gmail.com>
2019-09-30 11:49:39 +0100

    Redirect the users after logging in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mshop';
+
+  constructor(private auth: AuthService, router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        let returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
 }
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
+import {Router, CanActivate, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
 import 'rxjs/add/operator/map';
 
@@ -10,13 +10,13 @@ export class AuthGuardService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.map(user => {
       if (user) {
         return true;
       }
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
       return false;
     });
   }
