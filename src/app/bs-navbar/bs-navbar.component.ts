ba7d20b62d06c5c58818d663e685c5622184be4a
zerdan <zerdan.mohamed@gmail.com>
2019-09-30 10:02:36 +0100

    Extract login and logout as a service.

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AngularFireAuth} from 'angularfire2/auth';
-import * as firebase from 'firebase';
-import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-bs-navbar',
@@ -9,13 +7,11 @@ import {Observable} from 'rxjs';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
+  constructor(public auth: AuthService) {
   }
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 }
